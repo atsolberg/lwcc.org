e47096197b22d34d641eca6d55ab023df0c760de
atsolberg <atsolberg@gmail.com>
2022-08-10 17:11:01 -0500

    deleted everything not used

--- src/scripts/components/main/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './styles';
-
-function Main(props) {
-  return <main css={styles} className="container-fluid" {...props} />;
-}
-
-export default Main;
